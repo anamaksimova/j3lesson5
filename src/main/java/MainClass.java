d069d44cffe90b5c96e2b6101ecb6a852a432d3e
anamaksimova <19cherrytree91@gmail.com>
2021-03-23 21:41:31 +0500

    java3 lesson5

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,16 +1,38 @@
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class MainClass {
-    public static final int CARS_COUNT = 4;
+    public static final int CARS_COUNT =7;
+    public static final AtomicInteger finishCount = new AtomicInteger(0);
     public static void main(String[] args) {
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Semaphore semaphore = new Semaphore(CARS_COUNT/2);
+        final CountDownLatch cdl = new CountDownLatch(1);
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(CARS_COUNT+1);
+        Race race = new Race(new Road(60), new Tunnel(semaphore), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cyclicBarrier, cdl, finishCount);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        try {
+            cyclicBarrier.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            cyclicBarrier.await();
+            cdl.await();
+            System.out.println("Победитель известен!");
+            cyclicBarrier.await();
+           } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
